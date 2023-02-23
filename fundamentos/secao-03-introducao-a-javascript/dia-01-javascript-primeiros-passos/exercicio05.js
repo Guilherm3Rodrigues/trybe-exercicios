356562c398f8ec8ce2f44b51a62d9fcdd00d5847
guilherme <guilherme.rodrigues2@live.com>
2023-02-23 15:32:06 -0300

    exercicio 05 secao 03 javascript DIA 1, criação do switch e case, falta finalizar logica

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio05.js
@@ -0,0 +1,23 @@
+
+let escolha;
+
+switch (escolha) {
+    case "torre" : console.log("A torre anda apenas em angulos retos, esquerda, direita, frente, tras, a quantidade de casas que quiser"); break;
+
+    case "cavalo" : console.log("O cavalo anda apenas em L , 3 casas a frente (ou para o lado), e uma para o lado"); break;
+
+    case "bispo" : console.log("O bispo anda apenas na vertical, a quantidade de casas que quiser");
+    break;
+
+    case "rainha" : console.log("A rainha anda em qualquer direção a quantidade de casas que quiser");
+    break;
+
+    case "rei" : console.log("O Rei anda em qualquer direção, mas apenas uma casa por vez");
+    break;
+
+    case "peao" : console.log("O peão anda apenas para frente, uma casa por vez,");
+    break;
+
+    default: console.log("Peça invalida");
+}
+
