4a8475176bfee2c93d4e26181c6bfdf21346828d
guilherme <guilherme.rodrigues2@live.com>
2023-02-24 13:24:18 -0300

    dia 2 exercicios FOR-OF JS

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/For-of.js
@@ -0,0 +1,10 @@
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+    console.log(numero);
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+    for (let name of names) {
+        console.log(name);
+    }
