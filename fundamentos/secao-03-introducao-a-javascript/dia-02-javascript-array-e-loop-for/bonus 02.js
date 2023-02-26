e7956db36e83761fd032cf0a2e80acbf3de2472e
guilherme <guilherme.rodrigues2@live.com>
2023-02-26 11:21:43 -0300

    dia 2 bonus 02

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus 02.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus 02.js
@@ -1,10 +1,5 @@
 let array = [5, 3, 2, 4, 7, 1, 0, 6]
 
-
-
-
-
-
 // primeiro, como comparar e trocar 2 numeros de posição
 // segundo, como repetir o processo passando por todos os numeros da lista
 // terceiro, evitar repetições desnecessarias
@@ -14,7 +9,7 @@ for (let i2 = 0; i2 < array.length - 1; i2+= 1) {
 console.log("I2 ===============: " + i2);
   for (let i = 0; i < array.length - i2; i+= 1) {
     
-        if (array[i] > array[i+1]) {
+        if (array[i] < array[i+1]) {
             console.log("primeiro: " + array[i]);
             console.log("segundo: " + array[i+1]);
 
--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus 03.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus 03.js
@@ -1,10 +1,4 @@
 let array = [5, 3, 2, 4, 7, 1, 0, 6]
-
-
-
-
-
-
 // primeiro, como comparar e trocar 2 numeros de posição
 // segundo, como repetir o processo passando por todos os numeros da lista
 // terceiro, evitar repetições desnecessarias
