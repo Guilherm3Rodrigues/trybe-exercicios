abadf382c368b034c59c496a5c1ac9ae19d8e116
guilherme rodrigues <guilherme.rodrigues2@live.com>
2023-02-28 16:38:35 -0300

    finalizado aba de parametros

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula-ao-vivo/mega-sena.js
@@ -0,0 +1,15 @@
+// let lotteryNumbers = [];
+
+// for (let index = 1; index <= 60; index+= 1) {
+//     lotteryNumbers.push(index)
+
+// }
+
+// console.log(lotteryNumbers);
+
+let lotteryNumbers = [24, 32, 12, 8, 45, 57];
+
+for (let ind24, 32, 12, 8, 45, 57ex = 0; index < array.length; index+= 1) {
+    const element = array[index];
+    
+}
