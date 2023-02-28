fcc9634ec9b1c2979bfbcbe4dc67652ffcf380e0
guilherme rodrigues <guilherme.rodrigues2@live.com>
2023-02-28 16:23:15 -0300

    parametros exercicio incompleto

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/parametros-e-funcoes.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/parametros-e-funcoes.js
@@ -1,50 +1,69 @@
-let clientesCadastrados = ['ada', 'leon', 'wesker'];
-const sorvetes = ['chocolate', 'baunilha', 'flocus'];
+// let clientesCadastrados = ['ada', 'leon', 'wesker'];
+// const sorvetes = ['chocolate', 'baunilha', 'flocus'];
+
+// function validacaoDados(nome, sabor) {
+//     let cliente = false;
+//     let saborSorvete = false;
+
+//     for (let index = 0; index < clientesCadastrados.length; index += 1) {
+
+//         if (clientesCadastrados[index] === nome) {
+//             cliente = true;
+//             console.log("cliente cadastrado: " + nome);
+//             break;
+//         }
+//     }
+
+//     for (let index = 0; index < clientesCadastrados.length; index += 1) {
+
+//         if (sorvetes[index] === sabor) {
+
+//             saborSorvete = true;
+//             console.log("temos sorvete sabor: " + sabor);
+//             break;
+//         }
+//     }
+
+//     if (cliente === false) {
+//         if (saborSorvete === true) {
+//             console.log('cliente não cadastrado');
+//         } else {
+//             console.log('cliente nao cadastrado, e não temos esse sorvete');
+//         }
+//     } else {
+//         if (saborSorvete === false) {
+//             console.log('não temos esse sorvete');
+//         }
+//     }
+// }
+// validacaoDados('leon', 'flocus');
+
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+
+function cadastrarClientes (nome) {
 
 
 
-function validacaoDados(nome, sabor) {
-    let cliente = false;
-    let saborSorvete = false;
-
-    for (let index = 0; index < clientesCadastrados.length; index += 1) {
-
-        if (clientesCadastrados[index] === nome) {
-            cliente = true;
-            console.log("cliente cadastrado: " + nome);
-            break;
-        }
-
-
-    }
-
-    for (let index = 0; index < clientesCadastrados.length; index += 1) {
-
-        if (sorvetes[index] === sabor) {
-
-            saborSorvete = true;
-            console.log("temos sorvete sabor: " + sabor);
-            break;
-        }
-
-    }
-
-    if (cliente === false) {
-        if (saborSorvete === true) {
-            console.log('cliente não cadastrado');
-        } else {
-            console.log('cliente nao cadastrado, e não temos esse sorvete');
-        }
-    } else {
-        if (saborSorvete === false) {
-            console.log('não temos esse sorvete');
-        }
-    }
-
-
-
+    clientesTrybeBank.push(nome);
+    console.log(clientesTrybeBank);
 
 }
 
-validacaoDados('leon', 'flocus');
+cadastrarClientes('leon');
+
+function removerClientes (nome) {
+
+
+    for (let index = 0; index < clientesTrybeBank.length; index++) {
+        
+
+    }
+       console.log(clientesTrybeBank);   
+    
+}
+
+removerClientes('Gus');
+
 
