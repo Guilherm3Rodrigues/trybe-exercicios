27ddf0b2e1d8594a1bf79c575873ce94b8a1426d
guilherme rodrigues <guilherme.rodrigues2@live.com>
2023-02-28 21:49:01 -0300

    aulas ao vivo, megasena finalizada

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula-ao-vivo/mega-sena.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula-ao-vivo/mega-sena.js
@@ -7,9 +7,41 @@
 
 // console.log(lotteryNumbers);
 
-let lotteryNumbers = [24, 32, 12, 8, 45, 57];
+ let lotteryNumbers = [];
+ let firstGame = [16, 8, 60, 5, 7, 30]; 
+ let numberOfHits = 0;
+ let numbersWins = [];
 
-for (let index = 0; index < array.length; index+= 1) {
-    const element = array[index];
+    for (let index = 0; index < 6; index+= 1) {
+        let randomNumber = Math.ceil(Math.random() * 60);
+        
+        if (!lotteryNumbers.includes(randomNumber)) {
+            lotteryNumbers.push(randomNumber);
+        } else {
+            index -= 1;
+        }
+      
+
+
+}
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery +=1 ) {
+    
+    for (let indexGame = 0; indexGame < firstGame.length; indexGame +=1) {
+   
+        if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+            numberOfHits +=1;
+            numbersWins += firstGame[indexGame];
+            
+
+        } 
+  
     
 }
+}
+
+
+console.log('numeros da loteria: ' + lotteryNumbers);
+console.log('voce acertou o numero: ' + numbersWins );
+console.log('numero de acertos totais: ' + numberOfHits);
+console.log('seus numeros escolhidos: ' + firstGame);
+
