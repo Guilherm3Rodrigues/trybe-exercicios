ef7aad077b3f8a5a79992842b0e1dd184d6f05bd
guilherme rodrigues <guilherme.rodrigues2@live.com>
2023-03-01 18:08:00 -0300

    aula ao vivo dia 03 JS fim

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula-ao-vivo/mega-sena.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula-ao-vivo/mega-sena.js
@@ -7,41 +7,49 @@
 
 // console.log(lotteryNumbers);
 
- let lotteryNumbers = [];
- let firstGame = [16, 8, 60, 5, 7, 30]; 
- let numberOfHits = 0;
- let numbersWins = [];
+let lotteryNumbers = [];
+let firstGame = [];
+let numberOfHits = 0;
+let numbersWins = [];
 
-    for (let index = 0; index < 6; index+= 1) {
-        let randomNumber = Math.ceil(Math.random() * 60);
-        
-        if (!lotteryNumbers.includes(randomNumber)) {
-            lotteryNumbers.push(randomNumber);
+
+
+function generateLotteryNumbers(quantity, maxNumber, array) {
+    
+    for (let index = 0; index < quantity; index += 1) {
+        let randomNumber = Math.ceil(Math.random() * maxNumber);
+    
+        if (!array.includes(randomNumber)) {
+            array.push(randomNumber);
         } else {
             index -= 1;
         }
-      
-
-
-}
-for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery +=1 ) {
+    }
     
-    for (let indexGame = 0; indexGame < firstGame.length; indexGame +=1) {
-   
+}
+
+generateLotteryNumbers(6, 60, lotteryNumbers);
+generateLotteryNumbers(6, 60, firstGame);
+
+
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {
+
+    for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+
         if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
-            numberOfHits +=1;
+            numberOfHits += 1;
             numbersWins += firstGame[indexGame];
-            
 
-        } 
-  
-    
-}
+
+        }
+
+
+    }
 }
 
 
 console.log('numeros da loteria: ' + lotteryNumbers);
-console.log('voce acertou o numero: ' + numbersWins );
+console.log('voce acertou o numero: ' + numbersWins ,);
 console.log('numero de acertos totais: ' + numberOfHits);
 console.log('seus numeros escolhidos: ' + firstGame);
 
