7f23387f44dd03937c84f47c9086430ceadf46e7
guilherme rodrigues <guilherme.rodrigues2@live.com>
2023-03-03 09:31:21 -0300

    inicio exercicios do dia 04

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/object-keys-values-entries.js
@@ -0,0 +1,19 @@
+let animal = {
+    nome: 'Riuky',
+    idade: 2,
+    comida: 'ração'
+}
+
+let gato = {
+    nomeCaricatu: 'garfield',
+    idadeCaricatu: 15,
+    comidaCaricatu: 'lasanha'
+}
+
+
+let ass = animal.nome;
+
+
+
+console.log(animal['comida']);
+
